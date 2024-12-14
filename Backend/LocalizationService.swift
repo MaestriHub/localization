c8c0c16dd022bcfc14ed6ba1ea974aaff2c8def0
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-12-14 20:46:35 +0300

    пробую

--- Backend/LocalizationService.swift
+++ Backend/LocalizationService.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 // MARK: LocalizationService
 
@@ -29,8 +30,7 @@ public extension Request {
 }
 
 public actor LocalizationService: ILocalizationService {
-    static var localizeDirectory =
-    "./Localization"
+    static var localizeDirectory = #file
     //"./../Packages/MaestriCore/Sources/Services/LocalizationService/Localization"
     private let logger: Logger
     private let knowledge: LocalizeKnowledge
