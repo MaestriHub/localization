ddb5a7949e7f19fa3bdbcafd1066f9776d386636
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-12-14 20:58:11 +0300

    щас встанет

--- Backend/LocalizationCore/LocalizationFiles.swift
+++ Backend/LocalizationCore/LocalizationFiles.swift
@@ -6,6 +6,6 @@ public enum LocalizationFiles: String, CaseIterable {
     case salon = "salon"
     
     func path(ends: Lang) async -> String {
-        return "/\(rawValue)/\(ends.rawValue).json" // TODO:  вероятно можно придумать что-то лучше
+        return "\(rawValue)/\(ends.rawValue).json" // TODO:  вероятно можно придумать что-то лучше
     }
 }
