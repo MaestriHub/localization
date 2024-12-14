265d56b71ee8b64d1d92a3513a040d30204c585c
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-12-14 20:27:16 +0300

    проверка

--- Backend/LocalizationService.swift
+++ Backend/LocalizationService.swift
@@ -30,7 +30,7 @@ public extension Request {
 
 public actor LocalizationService: ILocalizationService {
     static var localizeDirectory =
-    "./Resources/Localization"
+    "./Localization"
     //"./../Packages/MaestriCore/Sources/Services/LocalizationService/Localization"
     private let logger: Logger
     private let knowledge: LocalizeKnowledge
