a5ef6a140a41fc3619fe3a86acb401806b99ac1c
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-12-16 21:33:12 +0300

    Update LocalizationFiles.swift

--- Backend/Provider/LocalizationFiles.swift
+++ Backend/Provider/LocalizationFiles.swift
@@ -4,8 +4,4 @@ enum LocalizationFiles: String, CaseIterable {
     case services = "services"
     case errors = "errors"
     case salon = "salon"
-    
-    func path() async -> String {
-        return "/\(rawValue).json" // TODO:  вероятно можно придумать что-то лучше
-    }
 }
