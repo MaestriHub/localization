f051bcf2cd94a9143c43828bd60cc0d642f20831
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-12-16 21:28:43 +0300

    почистил много что

--- Backend/Provider/LocalizationFiles.swift
+++ Backend/Provider/LocalizationFiles.swift
@@ -5,7 +5,7 @@ enum LocalizationFiles: String, CaseIterable {
     case errors = "errors"
     case salon = "salon"
     
-    func path(ends: Lang) async -> String {
-        return "/\(rawValue)/\(ends.rawValue).json" // TODO:  вероятно можно придумать что-то лучше
+    func path() async -> String {
+        return "/\(rawValue).json" // TODO:  вероятно можно придумать что-то лучше
     }
 }
--- Backend/Provider/Parsers/JsonParser.swift
+++ Backend/Provider/Parsers/JsonParser.swift
@@ -5,6 +5,8 @@ public struct JsonParser: ILocalizationParser {
     public init() {}
     
     public func getKnowledge(path: String) async -> LocalizeKnowledge {
+        let logger = Logger(label: "json.parser")
+        
         let servicePath = URL(fileURLWithPath: path)
         let resourcePath = servicePath
             .deletingLastPathComponent()
@@ -12,21 +14,40 @@ public struct JsonParser: ILocalizationParser {
             .path // TODO: для mac depreced для linux нужно
         
         var jsonKnowledge = LocalizeKnowledge()
-        for directory in LocalizationFiles.allCases {
-            for lang in Lang.allCases {
-                guard let data = try? await Data(contentsOf: URL(fileURLWithPath: resourcePath + directory.path(ends: lang))),
-                   let directoryLangMap = try? JSONDecoder().decode([String : String].self, from: data) else {
-                    continue
+        
+        for file in LocalizationFiles.allCases {
+            guard let dirLocalize = dirLocalize(path: resourcePath, file: file) else {
+                logger.error("not parsed path: \(resourcePath), file: \(file)")
+                continue
+            }
+            
+            dirLocalize.forEach({ langRaw, keyValuePairs in
+                guard let lang = Lang(rawValue: langRaw) else {
+                    logger.error("bad lang: \(langRaw)")
+                    return
                 }
-                
-                for (key, value) in directoryLangMap {
+                keyValuePairs.forEach({ key, value in
                     if jsonKnowledge[key] == nil {
                         jsonKnowledge[key] = [:]
                     }
                     jsonKnowledge[key]?[lang] = value
-                }
-            }
+                })
+            })
         }
         return jsonKnowledge
+    }
+    
+    private typealias ParsedStructure = [String: [String: String]]
+                                            
+    private func dirLocalize(path resourcePath: String, file directory: LocalizationFiles) -> ParsedStructure? {
+        let path = resourcePath + directory.rawValue + ".json"
+        let decoder = JSONDecoder()
+        
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+              let directoryLangMap = try? decoder.decode(ParsedStructure.self, from: data) else {
+            return nil
+        }
+        
+        return directoryLangMap
     }
 }
