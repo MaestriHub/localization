547513c80c59a4ae3e0722e009093e4ad353a0cc
chebupa <aristarhsakulin@gmail.com>
2024-12-27 01:44:30 +0300

    Update ProfessionalDashboardLocalization.swift

--- IOS/NewLocalization/Professional/Dashboard/ProfessionalDashboardLocalization.swift
+++ IOS/NewLocalization/Professional/Dashboard/ProfessionalDashboardLocalization.swift
@@ -41,7 +41,7 @@ public extension L10n.Professional.Dashboard.DangerZoneWidget {
         "professional.dashboard.danger_zone_widget.title",
         defaultValue: "Danger Zone",
         table: "ProfessionalDashboardLocalizable",
-        bundle: .current
+        bundle: .module
     )
     
     static let deactivateTitle = LocalizedStringResource(
