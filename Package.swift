0bb63933decc93895441a642315083d10e2c6e5f
chebupa <aristarhsakulin@gmail.com>
2024-12-28 10:05:52 +0300

    localizables folder rename

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
 //                .define("XCSTRINGS_TOOL_ACCESS_LEVEL_PUBLIC")
 //            ]
             resources: [
-                .copy("Localizables")
+                .copy("Resources")
             ]
         ),
         .target(
